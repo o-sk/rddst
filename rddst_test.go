48d1f936aff673c0f8a27d2e5ee23ce06332621a
o-sk <m.ohsuka@gmail.com>
2020-02-18 23:41:04 +0900

    Add test case

--- rddst_test.go
+++ rddst_test.go
@@ -26,7 +26,82 @@ func (m *mockClient) Head(url string) (*http.Response, error) {
 	return m.resp, m.err
 }
 
-func TestGetRedirectDestination(t *testing.T) {
+func TestGetRedirectDestinationNegative(t *testing.T) {
+	type mockParam struct {
+		resp *http.Response
+		err  error
+	}
+	type expect struct {
+		resp string
+		err  error
+	}
+	for _, tt := range []struct {
+		description string
+		mockParam   *mockParam
+		expect      *expect
+	}{
+		{
+			description: "The url is redirect url",
+			mockParam: &mockParam{
+				resp: &http.Response{
+					Request: &http.Request{
+						URL: func() *url.URL {
+							u, _ := url.Parse("https://redirect.example.com")
+							return u
+						}(),
+					},
+				},
+				err: nil,
+			},
+			expect: &expect{
+				resp: "https://redirect.example.com",
+				err:  nil,
+			},
+		},
+		{
+			description: "The url is not redirect url",
+			mockParam: &mockParam{
+				resp: &http.Response{
+					Request: &http.Request{
+						URL: func() *url.URL {
+							u, _ := url.Parse("https://example.com")
+							return u
+						}(),
+					},
+				},
+				err: nil,
+			},
+			expect: &expect{
+				resp: "https://example.com",
+				err:  nil,
+			},
+		},
+		{
+			description: "The http client return error",
+			mockParam: &mockParam{
+				resp: &http.Response{},
+				err:  xerrors.New("Something has occured"),
+			},
+			expect: &expect{
+				resp: "",
+				err:  xerrors.New("Something has occured"),
+			},
+		},
+	} {
+		t.Run(tt.description, func(t *testing.T) {
+			r := rddst.NewRddst(NewMockClient(tt.mockParam.resp, tt.mockParam.err))
+			resp, err := r.GetRedirectDestination("https://example.com", false)
+			assert.Equal(t, tt.expect.resp, resp)
+			if tt.expect.err != nil {
+				assert.Equal(t, tt.expect.err.Error(), err.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestGetRedirectDestinationPoositive(t *testing.T) {
 	type mockParam struct {
 		resp *http.Response
 		err  error
